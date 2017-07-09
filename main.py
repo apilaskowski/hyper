9925890f5734247181e4ab5e897c7230502fd49d
Artur Laskowski <artur.pi.laskowski@gmail.com>
2017-07-10 01:32:29 +0200

    ADD: Todo of whole basic program

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# TODO prepare few types of request to executioner
+# ask about number of heuristics(LLH)
+# ask data with evaluation
+# send data to single LLH, wait for respond
+# gather statistics about LLHs
+# when time passed, send result somewhere (??)
